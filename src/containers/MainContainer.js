a26583e8323acf9b0a2f393048bc7a6148c64d6b
Christopher Kim <chris.kim1990@gmail.com>
2021-06-01 11:51:10 -0400

    initial

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class MainContainer extends Component {
+
+
+    render() {
+        return (
+            <div className="main-body">
+                <h1>Hello2</h1>
+            </div>
+        )
+    }
+}
+
+export default MainContainer;
