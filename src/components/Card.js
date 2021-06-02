7ae505277a3797e5af71eef85b440c07147f4b31
Zecharias Mengis <zmengis@gmail.com>
2021-06-02 11:01:58 -0500

    updated card appearance

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,23 @@ import './Cards.css';
 const Card = ({card}) => {
     return (
         <div className="card">
-            <h1>{card.name}</h1>
-            <img src={card.image} alt={card.name} className="card-avatar"/>
-            <ul>
-                <li><b>HP:</b> {card.hp}</li>
-                <li><b>Ability:</b> {card.ability}</li>
-                <li><b>Ability Strength:</b> {card.ability_strength}</li>
-                <li><b>Rarity:</b> {card.rarity}</li>
-                <li><b>Holographic:</b> {card.holographic}</li>
-            </ul>
+            <div className="card-inner">
+                <div className="card-title">
+                    {card.name}
+                    <div className="card-hp-top">{card.hp}</div>
+                </div>
+                <img src={card.image} alt={card.name} className="card-avatar"/>
+                <div className="card-ability">
+                    {card.ability} 
+                </div>
+                <div className="card-description">
+                    A description goes here
+                </div>
+                <div className="card-ability-strength">{card.ability_strength}</div>
+                <div className="card-footer">
+                    Rarity: {card.rarity} / Holo: {card.holographic}
+                </div>
+            </div>
         </div>
              
     )
